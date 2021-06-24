0b4c1aaf6ed52f3eb0083ded419efec52839e645
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2021-06-24 13:10:36 +0200

    fixed filter

--- job-filter/dom-filter.js
+++ job-filter/dom-filter.js
@@ -1,32 +1,32 @@
-function createElement(item) {
-    const newDiv = document.createElement('div');
-    const existingDiv = document.querySelector('.referenceDiv');
-
-    //Storing the parent node in a variable
-    let parentDiv = existingDiv.parentNode;
-    const enterRegNum = document.createTextNode(item);
-    newDiv.appendChild(enterRegNum);
-    parentDiv.insertBefore(newDiv, existingDiv);
-    newDiv.classList.add('job');
-}
-
-
+let job1 = DisplayJobs();
 
 let informationTechnology = ['Job Title: Front-end developer Job description: Specializing in website and application design Skills required : HTML/CSS and Javscript Closing date : 10 July 2021 Location : 174 Freedom street, Johannesburg, 1912', 'Job Title: Computer Application Technology(CAT) Teacher Job description: Teaching Grade 10 to 12 learners (CAT), participating in extra mural activities and accompaning learners to school events  Skills required : BEd Teaching specialising in Computer Application Technology Closing date : 16 August 2021 Location : Sunward Park High School, Els Park, Boksburg', 'Job Title: Computer Lab assistant Job decription: Skills required: Closing date: 1 September 2021 Location: 23 College Avenue, Auckland Park, Johannesburg'];
 ['Job Title: Job decription: Skills required: Closing date: Location:']
+
+let teaching = ['teaching', 'Job Title: Job decription: Skills required: Closing date: Location:'];
+
+let nursing = ['nursing','Job Title: Job decription: Skills required: Closing date: Location:'];
+
+let accounting = ['accounting','Job Title: Job decription: Skills required: Closing date: Location:'];
+
+let engineering = ['engineering','Job Title: Job decription: Skills required: Closing date: Location:'];
 
 //HTML ELEMENTS
 const showBtn = document.querySelector('.showBtn');
 const chooseCategory = document.getElementById('chooseCategory');
 function categories() {
+    job1.clearJobs();
+    
     switch (chooseCategory.value) {
         case 'info-tech':
-            for (let i=0; i<informationTechnology.length; i++) {
-                let jobs = informationTechnology[i];
-                console.log(jobs);
-                createElement(jobs);
-            }
+            job1.displayCategoryJobs(informationTechnology);
             break;
+        case 'teaching':
+            job1.displayCategoryJobs(teaching);
+            break;
+        case 'nursing':
+            job1.displayCategoryJobs(nursing);
+            break;    
     }
 }
 
---
+++ job-filter/filter-factory-func.js
@@ -0,0 +1,36 @@
+function DisplayJobs() {
+    function createElement(item) {
+        const newDiv = document.createElement('div');
+        const existingDiv = document.querySelector('.referenceDiv');
+    
+        //Storing the parent node in a variable
+        let parentDiv = existingDiv.parentNode;
+        const enterRegNum = document.createTextNode(item);
+        newDiv.appendChild(enterRegNum);
+        parentDiv.insertBefore(newDiv, existingDiv);
+        newDiv.classList.add('job');
+    }
+
+    function displayCategoryJobs(jobList) {
+        for (let i=0; i<jobList.length; i++) {
+            let jobs = jobList[i];
+            console.log(jobs);
+            createElement(jobs);
+        }
+    }
+
+    function clearJobs() {
+    let node = document.querySelectorAll('.job');
+        for (let j=0; j<node.length; j++) {
+            if (node[j].parentNode) {
+                node[j].parentNode.removeChild(node[j]);
+            }; 
+        }
+    }
+
+    return {
+        createElement,
+        displayCategoryJobs,
+        clearJobs
+    }
+}
