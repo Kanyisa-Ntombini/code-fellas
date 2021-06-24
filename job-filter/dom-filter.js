40941835b799b3f6aa39e5356360520753ea3db5
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2021-06-24 11:21:01 +0200

    added changes to filter

--- job-filter/dom-filter.js
+++ job-filter/dom-filter.js
@@ -10,7 +10,10 @@ function createElement(item) {
     newDiv.classList.add('job');
 }
 
-let informationTechnology = ['Computer Lab Assistant', 'Computer Application Technology Teacher', 'Junior Web Developer'];
+
+
+let informationTechnology = ['Job Title: Front-end developer Job description: Specializing in website and application design Skills required : HTML/CSS and Javscript Closing date : 10 July 2021 Location : 174 Freedom street, Johannesburg, 1912', 'Job Title: Computer Application Technology(CAT) Teacher Job description: Teaching Grade 10 to 12 learners (CAT), participating in extra mural activities and accompaning learners to school events  Skills required : BEd Teaching specialising in Computer Application Technology Closing date : 16 August 2021 Location : Sunward Park High School, Els Park, Boksburg', 'Job Title: Computer Lab assistant Job decription: Skills required: Closing date: 1 September 2021 Location: 23 College Avenue, Auckland Park, Johannesburg'];
+['Job Title: Job decription: Skills required: Closing date: Location:']
 
 //HTML ELEMENTS
 const showBtn = document.querySelector('.showBtn');
