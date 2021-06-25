3318499b25b93b0be9a95709c763efa1a105ba4c
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2021-06-25 10:47:01 +0200

    added more jobs

--- job-filter/dom-filter.js
+++ job-filter/dom-filter.js
@@ -3,17 +3,19 @@ let job1 = DisplayJobs();
 let informationTechnology = [
     ['Job Title: Front-end Developer', 'Job description: Specializing in website and application design', 'Skills required : HTML/CSS and Javscript', 'Closing date : 10 July 2021', 'Location : 174 Freedom street, Johannesburg, 1912'],
     ['Job Title: Computer Application Technology(CAT) Teacher', 'Job description: Teaching Grade 10 to 12 learners CAT, participating in extra mural activities and accompaning learners to school events',  'Skills required : BEd Teaching specialising in Computer Application Technology', 'Closing date : 16 August 2021', 'Location : Sunward Park High School, Els Park, Boksburg'], 
-    ['Job Title: Computer Lab Assistant', 'Job description: ', 'Skills required:', 'Closing date: 1 September 2021', 'Location: 23 College Avenue, Auckland Park, Johannesburg'],
-    ['Job Title:', 'Job description:', 'Skills required:', 'Closing date:', 'Location:']
+    ['Job Title: Computer Lab Assistant', 'Job description: ', 'Skills required:', 'Closing date: 1 September 2021', 'Location: 23 College Avenue, Auckland Park, Johannesburg']
 ];
 
-let teaching = ['teaching', 'Job Title: Job decription: Skills required: Closing date: Location:'];
+let teaching = [
+    ['Job Title: Computer Application Technology(CAT) Teacher', 'Job description: Teaching Grade 10 to 12 learners CAT, participating in extra mural activities and accompaning learners to school events',  'Skills required : BEd Teaching specialising in Computer Application Technology', 'Closing date : 16 August 2021', 'Location : Sunward Park High School, Els Park, Boksburg'], 
+    ['Job Title: Music Teacher', 'Job description: Teaching the recorder to Grade 4 to 6 learners after school', 'Skills required: BA Music Degree specialising in any instrument', 'Closing date: 10 October 2021', 'Location: Balmoral Girls Primary School, Queenstown']
+];
 
-let nursing = ['nursing','Job Title: Job decription: Skills required: Closing date: Location:'];
+let nursing = [['Job Title: Nursing Home Assistant', 'Job description: Assist our residents with self-care activities such as getting dressed, making beds and eating', 'Skills required: Zero to no experience in nursing homes', 'Closing date: 11 August 2021', 'Location: Serenity Home, Southern Wood, East London']];
 
-let accounting = ['accounting','Job Title: Job decription: Skills required: Closing date: Location:'];
+let accounting = [['Job Title: Bookkeeper', 'Job decription: Preparing financial statements', 'Skills required: Matric certificate with Accounting as one of the subjects', 'Closing date: 30 November 2021', 'Location: Word of Life Bookshop, Grahamstown']];
 
-let engineering = ['engineering','Job Title: Job decription: Skills required: Closing date: Location:'];
+let engineering = [['Job Title: Junior Electrical Engineer', 'Job description: Designing the solar system of a new apartment development', 'Skills required: BEng/Bsc Electrical and/or Electronic Engineeering', 'Closing date: 30 December 2021', 'Location: 23 Main Road, Woodstock, Cape Town']];
 
 //Categories
 const showBtn = document.querySelector('.showBtn');
@@ -31,7 +33,13 @@ function categories() {
             break;
         case 'nursing':
             job1.displayCategoryJobs(nursing);
-            break;    
+            break;   
+        case 'accounting':
+            job1.displayCategoryJobs(accounting);
+            break; 
+        case 'engineering':
+            job1.displayCategoryJobs(engineering);
+            break; 
     }
 
     //creating the submit button
