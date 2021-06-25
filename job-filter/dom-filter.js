c7a4751edfabe94ed7a5634fdeccfd2bc40c1756
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2021-06-25 10:16:10 +0200

    completed the categories changes

--- job-filter/dom-filter.js
+++ job-filter/dom-filter.js
@@ -1,7 +1,11 @@
 let job1 = DisplayJobs();
 
-let informationTechnology = ['Job Title: Front-end developer Job description: Specializing in website and application design Skills required : HTML/CSS and Javscript Closing date : 10 July 2021 Location : 174 Freedom street, Johannesburg, 1912', 'Job Title: Computer Application Technology(CAT) Teacher Job description: Teaching Grade 10 to 12 learners (CAT), participating in extra mural activities and accompaning learners to school events  Skills required : BEd Teaching specialising in Computer Application Technology Closing date : 16 August 2021 Location : Sunward Park High School, Els Park, Boksburg', 'Job Title: Computer Lab assistant Job decription: Skills required: Closing date: 1 September 2021 Location: 23 College Avenue, Auckland Park, Johannesburg'];
-['Job Title: Job decription: Skills required: Closing date: Location:']
+let informationTechnology = [
+    ['Job Title: Front-end Developer', 'Job description: Specializing in website and application design', 'Skills required : HTML/CSS and Javscript', 'Closing date : 10 July 2021', 'Location : 174 Freedom street, Johannesburg, 1912'],
+    ['Job Title: Computer Application Technology(CAT) Teacher', 'Job description: Teaching Grade 10 to 12 learners CAT, participating in extra mural activities and accompaning learners to school events',  'Skills required : BEd Teaching specialising in Computer Application Technology', 'Closing date : 16 August 2021', 'Location : Sunward Park High School, Els Park, Boksburg'], 
+    ['Job Title: Computer Lab Assistant', 'Job description: ', 'Skills required:', 'Closing date: 1 September 2021', 'Location: 23 College Avenue, Auckland Park, Johannesburg'],
+    ['Job Title:', 'Job description:', 'Skills required:', 'Closing date:', 'Location:']
+];
 
 let teaching = ['teaching', 'Job Title: Job decription: Skills required: Closing date: Location:'];
 
@@ -11,14 +15,15 @@ let accounting = ['accounting','Job Title: Job decription: Skills required: Clos
 
 let engineering = ['engineering','Job Title: Job decription: Skills required: Closing date: Location:'];
 
-//HTML ELEMENTS
+//Categories
 const showBtn = document.querySelector('.showBtn');
 const chooseCategory = document.getElementById('chooseCategory');
 function categories() {
-    job1.clearJobs();
+    job1.clearElements();
     
     switch (chooseCategory.value) {
         case 'info-tech':
+            /*let jobAvailable = job1.loopThroughJobs(informationTechnology);*/
             job1.displayCategoryJobs(informationTechnology);
             break;
         case 'teaching':
@@ -28,6 +33,25 @@ function categories() {
             job1.displayCategoryJobs(nursing);
             break;    
     }
-}
 
+    //creating the submit button
+    let newButton = document.createElement('button');
+    const existingDiv = document.querySelector('.referenceDiv');
+            
+    //Storing the parent node in a variable
+    let parentDiv = existingDiv.parentNode;
+    const enterLabel = document.createTextNode('Click here to apply');
+    newButton.setAttribute('class','changeColor');
+    newButton.setAttribute('id', 'goToApplication')
+    newButton.appendChild(enterLabel);
+    parentDiv.insertBefore(newButton, existingDiv);
+
+    let goToApplication = document.getElementById('goToApplication');
+    function applicationButton () {
+        console.log(goToApplication);
+        window.open('https://www.ey.com/en_za/careers');
+        window.location.href = '../successfulApplication.html';
+    }
+    goToApplication.addEventListener('click', applicationButton);
+}
 showBtn.addEventListener('click', categories);
--- job-filter/filter-factory-func.js
+++ job-filter/filter-factory-func.js
@@ -1,29 +1,47 @@
 function DisplayJobs() {
-    function createElement(item) {
-        const newDiv = document.createElement('div');
-        let wrapped = document.createElement('a');
+    function createElement(jobAvailable) {
+        const newButton = document.createElement('button');
+        let wrapped1 = document.createElement('div');
+        let wrapped2 = document.createElement('div');
+        let wrapped3 = document.createElement('div');
+        let wrapped4 = document.createElement('div');
+        let wrapped5 = document.createElement('div');
         const existingDiv = document.querySelector('.referenceDiv');
             
         //Storing the parent node in a variable
         let parentDiv = existingDiv.parentNode;
-        const enterRegNum = document.createTextNode(item);
-        wrapped.setAttribute('href', 'https://www.ey.com/en_za/careers');
-        wrapped.appendChild(enterRegNum);
-        newDiv.appendChild(wrapped);
-        parentDiv.insertBefore(newDiv, existingDiv);
-        newDiv.classList.add('job');
+        const enterInfo1 = document.createTextNode(jobAvailable[0]);
+        const enterInfo2 = document.createTextNode(jobAvailable[1]);
+        const enterInfo3 = document.createTextNode(jobAvailable[2]);
+        const enterInfo4 = document.createTextNode(jobAvailable[3]);
+        const enterInfo5 = document.createTextNode(jobAvailable[4]);
+        wrapped1.appendChild(enterInfo1);
+        wrapped1.setAttribute('class', 'jobTitle');
+        wrapped2.appendChild(enterInfo2);
+        wrapped2.setAttribute('class', 'descriptions');
+        wrapped3.appendChild(enterInfo3);
+        wrapped3.setAttribute('class', 'descriptions');
+        wrapped4.appendChild(enterInfo4);
+        wrapped4.setAttribute('class', 'descriptions');
+        wrapped5.appendChild(enterInfo5);
+        newButton.appendChild(wrapped1);
+        newButton.appendChild(wrapped2);
+        newButton.appendChild(wrapped3);
+        newButton.appendChild(wrapped4);
+        newButton.appendChild(wrapped5);
+        parentDiv.insertBefore(newButton, existingDiv);
+        newButton.classList.add('changeColor');
     }
 
     function displayCategoryJobs(jobList) {
         for (let i=0; i<jobList.length; i++) {
             let jobs = jobList[i];
-            console.log(jobs);
             createElement(jobs);
         }
     }
 
-    function clearJobs() {
-    let node = document.querySelectorAll('.job');
+    function clearElements() {
+    let node = document.querySelectorAll('.changeColor');
         for (let j=0; j<node.length; j++) {
             if (node[j].parentNode) {
                 node[j].parentNode.removeChild(node[j]);
@@ -31,9 +49,14 @@ function DisplayJobs() {
         }
     }
 
+    function addClass(element) {
+        element.setAttribute('href','somelink')
+    }
+
     return {
         createElement,
         displayCategoryJobs,
-        clearJobs
+        clearElements,
+        addClass
     }
 }
